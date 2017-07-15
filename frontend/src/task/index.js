3f7988d22a19d0ffead4e43cf53dd35bbeb1ac36
Cadell Christo <cadell.christo@velpic.com>
2017-07-15 14:08:58 +0800

    moved the collection request to a higher component so the item view can refresh it

--- frontend/src/task/index.js
+++ frontend/src/task/index.js
@@ -4,30 +4,62 @@ import { AppBar } from "material-ui";
 import ContentAdd from "material-ui/svg-icons/content/add";
 import IconButton from "material-ui/IconButton";
 import SidebarStore from "sidebar/store";
+import { action, observable } from "mobx";
+import { observer } from "mobx-react";
+import { fromPromise } from "mobx-utils";
+import axios from "axios";
 
 import List from "./list";
 import Item from "./item";
 
-const Task = ({ match }) =>
-	<div className="row no-gutters">
-		<div className="col h-100vh">
-			<AppBar
-				title="Tasks"
-				onLeftIconButtonTouchTap={SidebarStore.toggle}
-				iconElementRight={
-					<Link to={`${match.path}/create`}>
-						<IconButton>
-							<ContentAdd />
-						</IconButton>
-					</Link>
-				}
-			/>
-			<List />
-		</div>
-		<Route
-			path={`${match.path}/:id`}
-			render={props => <Item {...props} className="col h-100vh" />}
-		/>
-	</div>;
+@observer
+class TaskIndex extends Component {
+	@observable request;
 
-export default Task;
+	isLargeScreen = (props = this.props) => {
+		return true;
+	};
+
+	load = () => {
+		this.request = fromPromise(axios.get("tasks"));
+	};
+
+	render() {
+		const request = this.request;
+		const { match } = this.props;
+		return (
+			<div className="row no-gutters">
+				<Route
+					path={`${match.path}`}
+					exact={!this.isLargeScreen()}
+					render={props =>
+						<div className="col h-100vh">
+							<AppBar
+								title="Tasks"
+								onLeftIconButtonTouchTap={SidebarStore.toggle}
+								iconElementRight={
+									<Link to={`${match.path}/create`}>
+										<IconButton>
+											<ContentAdd />
+										</IconButton>
+									</Link>
+								}
+							/>
+							<List load={this.load} request={request} />
+						</div>}
+				/>
+				<Route
+					path={`${match.path}/:id`}
+					render={props =>
+						<Item
+							{...props}
+							className="col h-100vh"
+							load={this.isLargeScreen() ? this.load : undefined}
+						/>}
+				/>
+			</div>
+		);
+	}
+}
+
+export default TaskIndex;
--- frontend/src/task/list.js
+++ frontend/src/task/list.js
@@ -3,26 +3,59 @@ import { autorun } from "mobx";
 import { observer } from "mobx-react";
 import { fromPromise } from "mobx-utils";
 import axios from "axios";
+import {
+	Table,
+	TableBody,
+	TableHeader,
+	TableHeaderColumn,
+	TableRow,
+	TableRowColumn,
+} from "material-ui/Table";
 
 @observer
 class list extends Component {
-	constructor() {
-		super();
-		autorun(this.fetch);
+	componentDidMount() {
+		this.props.load();
 	}
 
-	fetch = () => {
-		this.request = fromPromise(axios.get("tasks"));
-	};
+	table(result) {
+		return (
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHeaderColumn>ID</TableHeaderColumn>
+						<TableHeaderColumn>Name</TableHeaderColumn>
+						<TableHeaderColumn>Notes</TableHeaderColumn>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{result.data.content.map(row =>
+						<TableRow key={row.id}>
+							<TableRowColumn>
+								{row.id}
+							</TableRowColumn>
+							<TableRowColumn>
+								{row.name}
+							</TableRowColumn>
+							<TableRowColumn>
+								{row.notes}
+							</TableRowColumn>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+		);
+	}
 
 	render() {
+		const { request } = this.props;
 		return (
 			<div>
-				{this.request &&
-					this.request.case({
+				{request &&
+					request.case({
 						pending: () => <div>Loading...</div>,
 						rejected: error => <div>Ooops..</div>,
-						fulfilled: result => <div>LOADED!</div>,
+						fulfilled: result => this.table(result),
 					})}
 			</div>
 		);
