03a3bd8ec4ecff479668597427dc658f509574b1
Cadell Christo <cadell.christo@velpic.com>
2018-07-13 21:10:35 +1000

    use checkbox for selection instead of radio

--- ui/src/main/authenticated/user/index.js
+++ ui/src/main/authenticated/user/index.js
@@ -25,7 +25,7 @@ class User extends Component {
 					exact={singleView}
 					render={props => (
 						<Page>
-							<AppBar position="static">
+							<AppBar>
 								<MenuButton toggleSideBar={toggleSideBar} />
 								<AppBarTitle>Users</AppBarTitle>
 								<RefreshButton findAll={findAll} />
