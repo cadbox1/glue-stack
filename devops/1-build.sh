e09d0381ef258044d345f7ef8798b02d9b0edc2b
Cadell Christo <cadellchristo>
2019-09-17 05:16:25 +0800

    configure project root directory

--- devops/1-build.sh
+++ devops/1-build.sh
@@ -2,16 +2,18 @@
 
 set -e
 
+PROJECT_ROOT="${BASH_SOURCE%/*}/.."
+
 gcloud auth configure-docker --quiet
 
 APP_IMAGE_TAG="asia.gcr.io/glue-stack-251212/app"
 
 docker pull $APP_IMAGE_TAG:api-compile || true
 
-docker build --cache-from $APP_IMAGE_TAG:api-compile --target api-compile -t $APP_IMAGE_TAG:api-compile ../
+docker build --cache-from $APP_IMAGE_TAG:api-compile --target api-compile -t $APP_IMAGE_TAG:api-compile "${PROJECT_ROOT}/"
 docker push $APP_IMAGE_TAG:api-compile
 
-docker run -it --rm -v $PWD/../api/:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
+docker run -it --rm -v "${PROJECT_ROOT}/api/":$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
 
-docker build --cache-from $APP_IMAGE_TAG:api-compile -t $APP_IMAGE_TAG:latest ../
+docker build --cache-from $APP_IMAGE_TAG:api-compile -t $APP_IMAGE_TAG:latest "${PROJECT_ROOT}/"
 docker push $APP_IMAGE_TAG:latest
