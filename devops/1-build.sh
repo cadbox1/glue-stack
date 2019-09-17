b790faa007a014e8234c5cccca350a704140973f
Cadell Christo <cadellchristo>
2019-09-17 18:34:21 +0800

    mount the volume to the default directory

--- devops/1-build.sh
+++ devops/1-build.sh
@@ -11,7 +11,7 @@ docker pull $APP_IMAGE_TAG:api-compile || true
 docker build --cache-from $APP_IMAGE_TAG:api-compile --target api-compile -t $APP_IMAGE_TAG:api-compile ../
 docker push $APP_IMAGE_TAG:api-compile
 
-docker run --rm -v $PWD/../api/:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
+docker run --rm -v $PWD/../api/:/ -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
 
 docker build --cache-from $APP_IMAGE_TAG:api-compile -t $APP_IMAGE_TAG:latest ../
 docker push $APP_IMAGE_TAG:latest
