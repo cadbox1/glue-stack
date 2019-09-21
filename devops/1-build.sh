40cd2d6929623fe01cfdd81d162148da31dcf837
Cadell Christo <cadellchristo>
2019-09-21 14:26:09 +1000

    use correct bind mount when inside a container

--- devops/1-build.sh
+++ devops/1-build.sh
@@ -11,7 +11,14 @@ docker pull $APP_IMAGE_TAG:api-compile || true
 docker build --cache-from $APP_IMAGE_TAG:api-compile --target api-compile -t $APP_IMAGE_TAG:api-compile ../
 docker push $APP_IMAGE_TAG:api-compile
 
-docker run --rm -v $PWD/../api:/api -w /api -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
+# https://stackoverflow.com/questions/55845723/volume-bind-mounting-from-docker-in-docker
+
+if [ -z ${GITHUB_WORKSPACE+x} ]; 
+	then PROJECT_ROOT="$PWD/.."; 
+	else PROJECT_ROOT=$GITHUB_WORKSPACE; 
+fi
+
+docker run --rm -v $PROJECT_ROOT/api:/api -w /api -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
 
 docker build --cache-from $APP_IMAGE_TAG:api-compile -t $APP_IMAGE_TAG:latest ../
 docker push $APP_IMAGE_TAG:latest
