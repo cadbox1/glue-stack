0179f73638893fc69be494f45e4befbea098e2ea
Cadell Christo <cadellchristo>
2019-09-22 12:58:18 +1000

    current progress

--- devops/1-build.sh
+++ devops/1-build.sh
@@ -4,7 +4,9 @@ set -ex
 
 gcloud auth configure-docker --quiet
 
-APP_IMAGE_TAG="asia.gcr.io/glue-stack-251212/app"
+REPO="asia.gcr.io/glue-stack-251212"
+APP_IMAGE_TAG="$REPO/app-compiled"
+APP_IMAGE_TAG="$REPO/app"
 
 docker pull $APP_IMAGE_TAG:api-compile || true
 
@@ -12,13 +14,12 @@ docker build --cache-from $APP_IMAGE_TAG:api-compile --target api-compile -t $AP
 docker push $APP_IMAGE_TAG:api-compile
 
 # https://stackoverflow.com/questions/55845723/volume-bind-mounting-from-docker-in-docker
-
-if [ -z ${GITHUB_WORKSPACE+x} ]; 
-	then PROJECT_ROOT="$PWD/.."; 
-	else PROJECT_ROOT=$GITHUB_WORKSPACE; 
+if [ -z ${GITHUB_WORKSPACE+x} ]
+	then PROJECT_ROOT="$PWD/.."
+	else PROJECT_ROOT="/home/runner/work/glue-stack/glue-stack"
 fi
 
-docker run --rm -v /home/runner/work/glue-stack/glue-stack/api:/api -w /api -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
+docker run --rm -v $PROJECT_ROOT/api:/api -w /api -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
 
 docker build --cache-from $APP_IMAGE_TAG:api-compile -t $APP_IMAGE_TAG:latest ../
 docker push $APP_IMAGE_TAG:latest
