54f7ca0127ba97afffbaea5e2202dbd48448f7fc
Cadell Christo <cadellchristo>
2019-09-23 21:37:42 +1000

    setup an nginx service

--- devops/1-build.sh
+++ devops/1-build.sh
@@ -2,24 +2,11 @@
 
 set -ex
 
-gcloud auth configure-docker --quiet
+cd ../
+PROJECT_ROOT=$PWD
 
-REPO="asia.gcr.io/glue-stack-251212"
-APP_IMAGE_TAG="$REPO/app-compiled"
-APP_IMAGE_TAG="$REPO/app"
+cd $PROJECT_ROOT/api/devops
+sh ./build.sh
 
-docker pull $APP_IMAGE_TAG:api-compile || true
-
-docker build --cache-from $APP_IMAGE_TAG:api-compile --target api-compile -t $APP_IMAGE_TAG:api-compile ../
-docker push $APP_IMAGE_TAG:api-compile
-
-# https://stackoverflow.com/questions/55845723/volume-bind-mounting-from-docker-in-docker
-if [ -z ${GITHUB_WORKSPACE+x} ]
-	then PROJECT_ROOT="$PWD/.."
-	else PROJECT_ROOT="/home/runner/work/glue-stack/glue-stack"
-fi
-
-docker run --rm -v $PROJECT_ROOT/api:/api -w /api -v /var/run/docker.sock:/var/run/docker.sock $APP_IMAGE_TAG:api-compile mvn test
-
-docker build --cache-from $APP_IMAGE_TAG:api-compile -t $APP_IMAGE_TAG:latest ../
-docker push $APP_IMAGE_TAG:latest
+cd $PROJECT_ROOT/ui/devops
+sh ./build.sh
