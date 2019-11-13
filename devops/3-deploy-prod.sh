379afe67df9409d7caece227efdc26b49498a57c
Cadell Christo <cadellchristo>
2019-11-13 21:51:41 +0800

    only specify the minor kubernetes cluster version

--- devops/3-deploy-prod.sh
+++ devops/3-deploy-prod.sh
@@ -6,7 +6,7 @@ gcloud config set project glue-stack-251212
 gcloud config set compute/zone australia-southeast1-a
 
 # create prod cluster. will fail if it already exists but that's ok.
-gcloud container clusters create prod-cluster --cluster-version=1.13.7-gke.24 --num-nodes=1 --machine-type=g1-small || true
+gcloud container clusters create prod-cluster --cluster-version=1.13 --num-nodes=1 --machine-type=g1-small || true
 
 gcloud container clusters get-credentials prod-cluster
 
